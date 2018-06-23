312f067f2161c139834d424cb937d1e873460f4f
gladavis89 <gla.davis89@gmail.com>
2018-06-23 10:56:56 -0700

    adjusted search function, added on(click)

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,1 +1,20 @@
-$
+$("#submit").click(function(){
+
+console.log($("#drop").val())
+
+var artist = 'artist.search&artist=nirvana'
+var album = 'album.search&album='
+var option = 'track.search&track=feel good inc'
+
+
+
+$.ajax({
+  url: "http://ws.audioscrobbler.com//2.0/?method=track.search&track=feel good inc&api_key=5a85a72189c33df2b02663c378c7f775&format=json",
+  method: "GET"
+}).then(function(response) {
+  console.log(response)
+
+
+
+})
+})
