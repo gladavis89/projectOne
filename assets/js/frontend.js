58b1fdb2e2617683b56ffc2a79738bbce75d6b5d
gladavis89 <gla.davis89@gmail.com>
2018-06-23 12:18:20 -0700

    made search work with lastfm

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,15 +1,17 @@
 $("#submit").click(function(){
 
 console.log($("#drop").val())
-
-var artist = 'artist.search&artist=nirvana'
-var album = 'album.search&album='
-var option = 'track.search&track=feel good inc'
+console.log($("#input").val())
+var input = $('#input').val()
+var option = $("#drop").val()
+var artist = 'artist.getInfo&artist='
+var album = 'album.getInfo&album='
+var song = 'track.getInfo&track='
 
 
 
 $.ajax({
-  url: "http://ws.audioscrobbler.com//2.0/?method=track.search&track=feel good inc&api_key=5a85a72189c33df2b02663c378c7f775&format=json",
+  url: "http://ws.audioscrobbler.com//2.0/?method="+option+".getInfo&"+option+"="+input+"&api_key=5a85a72189c33df2b02663c378c7f775&format=json",
   method: "GET"
 }).then(function(response) {
   console.log(response)
